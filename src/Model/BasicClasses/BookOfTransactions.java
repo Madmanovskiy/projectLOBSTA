13548a488c95ed883ff4606e741b23a9c380b357
Maks <madmanovskiy@yandex.ru>
2015-07-28 10:25:20 +0300

    Change addDealMethod to addEzcOrdToDealsList - return counter of added deals; add method IO without realization;

--- src/Model/BasicClasses/BookOfTransactions.java
+++ src/Model/BasicClasses/BookOfTransactions.java
@@ -22,13 +22,36 @@ public class BookOfTransactions {
         orders.add(order);
     }
 
-    public void updateListsData() {
+    //return the count of executed orders
+    public int addExecutedOrderToDealsList() {
+        int counter = 0;
         for (UserOrder order : orders){
             if (order.isExecuted()) {
                 deals.add(order);
+                counter++;
             }
         }
+        return counter;
     }
+
+    //IO to\from file, hasn't realization yet
+    public void writeOrdersToFile(){
+
+    }
+
+    public void writeDealsToFile(){
+
+    }
+
+    public void readOrdersFromFile(){
+
+    }
+
+    public void readDealsFromFile(){
+
+    }
+
+
 
     public static synchronized BookOfTransactions getInstance() {
         if (instance == null) instance = new BookOfTransactions();
