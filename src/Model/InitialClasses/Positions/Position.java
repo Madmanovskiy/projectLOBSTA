0c68aad1a813138ad85f587bfc0d8bebdb4c4b5c
Maks <madmanovskiy@yandex.ru>
2015-08-07 18:12:43 +0300

    added methods to Position and changed variables

--- src/Model/InitialClasses/Positions/Position.java
+++ src/Model/InitialClasses/Positions/Position.java
@@ -2,22 +2,36 @@ package Model.InitialClasses.Positions;
 
 
 import Model.InitialClasses.Other.Futures;
+import Model.InitialClasses.Other.NinjaTraderTerminal;
 
 public class Position {
     private final int id;
     private final Futures futures;
-    private int currentContractsAmount;
-    private double cashValue;
-    private double buyPower;
-    private double profitLoss;
+    private final String account;
 
-    public Position(int id, Futures futures, int currentContractsAmount) {
-        this.futures = futures;
+    public Position(int id, String account, Futures futures) {
         this.id = id;
-        this.currentContractsAmount = currentContractsAmount;
+        this.account = account;
+        this.futures = futures;
     }
 
+    public double getBuyPower() {
+        return NinjaTraderTerminal.INSTANCE.BuyingPower(account);
+    }
 
+    public double getCashValue() {
+        return NinjaTraderTerminal.INSTANCE.CashValue(account);
+    }
 
+    public int getCurrentContractsAmount() {
+        return NinjaTraderTerminal.INSTANCE.MarketPosition(futures.getTicker(), account);
+    }
 
+    public double getProfitLoss() {
+        return NinjaTraderTerminal.INSTANCE.RealizedPnL(account);
+    }
+
+    public Futures getFutures() {
+        return futures;
+    }
 }
