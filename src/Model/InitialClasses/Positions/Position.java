061af92b87e6e4127bd812a672aad830cba6f28e
Maks <madmanovskiy@yandex.ru>
2015-08-07 19:32:28 +0300

    add new methods (calculate com. costs\pos value etc)

--- src/Model/InitialClasses/Positions/Position.java
+++ src/Model/InitialClasses/Positions/Position.java
@@ -7,17 +7,28 @@ import Model.InitialClasses.Other.NinjaTraderTerminal;
 public class Position {
     private final Futures futures;
     private final String account;
+    private final double priceEnter;
 
-    protected Position(String account, Futures futures) {
+    protected Position(String account, Futures futures, double priceEnter) {
         this.account = account;
         this.futures = futures;
+        this.priceEnter = priceEnter;
     }
 
-    protected int getCurrentContractsAmount() {
+    protected int getCurrentContractsQuantity() {
         return NinjaTraderTerminal.INSTANCE.MarketPosition(futures.getTicker(), account);
     }
 
     protected Futures getFutures() {
         return futures;
+    }
+
+    protected double currentPositionsResult(double currentPrice) {
+        if (getCurrentContractsQuantity() == 0) return 0;
+        return getCurrentContractsQuantity() > 0 ? currentPrice - priceEnter : priceEnter - currentPrice;
+    }
+
+    protected double commissionCosts(){
+        return futures.getAllCommissionCosts() * getCurrentContractsQuantity();
     }
 }
--- src/Model/InitialClasses/Positions/PositionManager.java
+++ src/Model/InitialClasses/Positions/PositionManager.java
@@ -12,7 +12,6 @@ public class PositionManager {
     private static PositionManager instance;
     private static Map<String, Position> positionMap = new HashMap();
     private final String account;
-    private double commissionCosts;
 
     private PositionManager(String account) {
         this.account = account;
@@ -30,34 +29,52 @@ public class PositionManager {
         return NinjaTraderTerminal.INSTANCE.MarketPosition(account, futures.getTicker()) != 0;
     }
 
-    public void createPosition(String account, Futures futures) {
-        positionMap.put(futures.getTicker(), new Position(account, futures));
-        commissionCosts += positionMap.get(futures.getTicker()).getCurrentContractsAmount() * futures.getAllCommissionCosts();
+    public void createPosition(String account, Futures futures, double priceEnter) {
+        positionMap.put(futures.getTicker(), new Position(account, futures, priceEnter));
     }
 
     public boolean isPositionClosed(Futures futures) {
-        return positionMap.get(futures.getTicker()).getCurrentContractsAmount() == 0;
+        return positionMap.get(futures.getTicker()).getCurrentContractsQuantity() == 0;
     }
 
     public void removePosition(Futures futures) {
         positionMap.remove(futures.getTicker());
     }
 
+
     public double getBuyPower() {
-        return NinjaTraderTerminal.INSTANCE.BuyingPower(account) - commissionCosts;
+        return NinjaTraderTerminal.INSTANCE.BuyingPower(account);
     }
 
     public double getCashValue() {
-        return NinjaTraderTerminal.INSTANCE.CashValue(account) - commissionCosts;
+        return NinjaTraderTerminal.INSTANCE.CashValue(account);
     }
 
-    protected double getProfitLoss() {
-        return NinjaTraderTerminal.INSTANCE.RealizedPnL(account) - commissionCosts;
+
+    public double getPositionResult(Futures futures, double currentPrice) {
+        Position pos = positionMap.get(futures.getTicker());
+        return pos.currentPositionsResult(currentPrice) - pos.getCurrentContractsQuantity() - pos.commissionCosts();
+    }
+
+    public double getSummaryCommissionCosts(){
+        double commissions = 0;
+        if (positionMap.isEmpty()) return 0;
+
+        for (Map.Entry<String, Position> pair : positionMap.entrySet()){
+            commissions+= pair.getValue().commissionCosts();
+        }
+
+        return commissions;
+    }
+
+    public double getSummaryProfitLoss() {
+        return NinjaTraderTerminal.INSTANCE.RealizedPnL(account) - getSummaryCommissionCosts();
     }
 
     public int getPositionQuantity(Futures futures) {
-        return positionMap.get(futures.getTicker()).getCurrentContractsAmount();
+        return positionMap.get(futures.getTicker()).getCurrentContractsQuantity();
     }
+
 
     public double getPositionValue(Futures futures) {
         return getPositionQuantity(futures) * futures.getGuaranteeSumLower();
